14e254ecc1054797509f3b0a671df5b9a8c4f83a
Evmilk <Evmilk@users.noreply.github.com>
2018-10-12 17:14:58 +0800

    Cocos Creator
    事件传递

---
+++ clientEvent.js
@@ -0,0 +1,37 @@
+window.clientEvent = {
+    eventType: {
+        //小狗商店新加脚本
+        showDogTips: "showDogTips",
+    },
+    eventListener: null
+}
+
+clientEvent.init = function() {
+    console.log("监听成功");
+    clientEvent.eventListener = eventListener.create();
+};
+
+clientEvent.on = function(eventName, handler, target) {
+    if (typeof eventName !== "string") {
+        return;
+    }
+    clientEvent.eventListener.on(eventName, handler, target);
+};
+
+clientEvent.off = function(eventName, handler, target) {
+    if (typeof eventName !== "string") {
+        return;
+    }
+    clientEvent.eventListener.off(eventName, handler, target);
+};
+
+clientEvent.clear = function(target) {
+    clientEvent.eventListener.clear(target);
+};
+
+clientEvent.dispatch = function(eventName, data) {
+    if (typeof eventName !== "string") {
+        return;
+    }
+    clientEvent.eventListener.dispatch(eventName, data);
+};
